66fc445e20c856b92ed6c49f5296ca65ce4333ae
malakhshou <malakah.shalhoub@x64-corp.com>
2017-06-27 16:43:31 +0530

    Update appengine_config.py

--- appengine_config.py
+++ appengine_config.py
@@ -6,3 +6,10 @@ if os.environ.get('SERVER_SOFTWARE','').startswith('Dev'):
     sys.path.append('/Users/matsumotokoyo/python/out/out')
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "out.settings")
+
+#APIKEYS
+consumer_api = "TLMx9b2GcPAeUBMElKY8PwJhE"
+consumer_secret = "IRUF7n9Q0idV5QOzGMrDgT75LKqLK8OkscrxAUYg1dJV1NCmWp"
+access_token = "876140352347840513-15aKHglha33QnmoAyiTZH3lFl9FUVMu"
+access_token_secret = "uyMP91Dk2yI6aCbC2ybxQ9ycXak57WNHH3QxsT9hJtGNX"
+
